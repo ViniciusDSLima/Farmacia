0841d9a926e09f1dcf3e5707a3730c526c6c27c3
Vinicius <vinicius.devjvm@gmail.com>
2023-07-20 12:09:00 -0300

    criada a configuracao de Cors

--- src/main/java/com/Aplicacao/Loja/infra/security/SecurityConfigurarion.java
+++ src/main/java/com/Aplicacao/Loja/infra/security/SecurityConfigurarion.java
@@ -1,18 +1,22 @@
 package com.Aplicacao.Loja.infra.security;
 
+import org.hibernate.cache.spi.support.CollectionNonStrictReadWriteAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -80,4 +84,18 @@ public class SecurityConfigurarion {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public Customizer<CorsConfigurer<HttpSecurity>> corsCustomize(){
+        return (cors) -> cors.configurationSource(request -> {
+            CorsConfiguration configuration  =  new CorsConfiguration();
+            configuration.addAllowedOrigin("http://localhost:8080");
+            configuration.addAllowedMethod("*");
+            configuration.addAllowedHeader("*");
+            configuration.setAllowCredentials(true);
+            return configuration;
+        });
+
+    }
+
 }
